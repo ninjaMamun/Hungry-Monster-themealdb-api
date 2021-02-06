e7705501f0957c512f7a4f8f78af0d971f5999f1
Mamun <shahriyar.m.mamun@gmail.com>
2021-02-07 03:07:51 +0600

    half work done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const getMealList = () =>{
     let searchInput = document.getElementById('searchInput').value;
     let parentNode = document.getElementById('showItems');
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+    let NotFoundParentNode = document.getElementById('notfound');
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
     .then(response => response.json())
     .then(data => {
         if(data.meals){
@@ -9,16 +10,17 @@ const getMealList = () =>{
                 console.log(meal.idMeal);
                 console.log(meal.strMeal);
                 const mealItemDiv = document.createElement('div');
+                mealItemDiv.className = 'col-md-3 item-columns';
                 
                 let cardHtml = `
-                <div class="card" style="width: 18rem;">
+                <div class="card rounded-3 border-0">
                     <img src = "${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">${meal.strMeal}</h5>
+                      <h5 class="card-title food-title text-center">${meal.strMeal}</h5>
                     </div>
                   </div>
               
-                `
+                `;
                 mealItemDiv.innerHTML = cardHtml;
                 parentNode.appendChild(mealItemDiv);
 
@@ -26,10 +28,27 @@ const getMealList = () =>{
 
 
             });
+        }else{
+            const mealItemDiv = document.createElement('div');
+            mealItemDiv.className = 'col-md-3 item-columns';
+            const notFoundCard = `
+            <div class="card" style="width: 18rem;">
+            <img src="./images/undraw_empty_xct9.svg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Not Found!</h5>
+                <p class="card-text">Please Try Again With Correct Keyword!</p>
+            </div>
+            </div>
+             `;
+             mealItemDiv.innerHTML = notFoundCard;
+             NotFoundParentNode.appendChild(mealItemDiv);
+
         }
     })
 }
 document.getElementById('searchButton').addEventListener('click', function(){
+
     getMealList();
+
 })
 
