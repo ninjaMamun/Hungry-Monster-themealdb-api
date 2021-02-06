cd3ca22a33dda00757391b9d5e6cc49c3b6eb298
Mamun <shahriyar.m.mamun@gmail.com>
2021-02-07 01:34:48 +0600

    some works done

---
+++ app.js
@@ -0,0 +1,34 @@
+const getMealList = () =>{
+    let searchInput = document.getElementById('searchInput').value;
+    let parentNode = document.getElementById('showIteams');
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+        if(data.meals){
+            data.meals.forEach(meal => {
+                console.log(meal.idMeal);
+                console.log(meal.strMeal);
+                const mealItemDiv = document.createElement('div');
+                
+                let cardHtml = `
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${meal.strMeal}</h5>
+                    </div>
+                  </div>
+              
+                `
+                mealItemDiv.innerHTML = cardHtml;
+                parentNode.appendChild(mealItemDiv);
+
+
+
+
+            });
+        }
+    })
+}
+document.getElementById('searchButton').addEventListener('click', function(){
+    getMealList();
+})
